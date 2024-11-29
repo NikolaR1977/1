434b4982b6529f407e4c4f961595b4933f524a33
Nikolay <hokumr@gmail/com>
2024-11-29 20:03:39 +0300

    init

--- homework/average score.py
+++ homework/average score.py
@@ -3,3 +3,4 @@ print(list(sorted(students)))
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 average=sum(grades [0])/len(grades[0]),sum(grades [1])/len(grades[1]),sum(grades [2])/len(grades[2]),sum(grades [3])/len(grades[3])
 print(average)
+print()
