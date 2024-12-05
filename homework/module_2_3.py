3a2cfeea4f075b7c5cf808d000dd91a53f9b2233
Nikolay <hokumr@gmail/com>
2024-12-05 18:59:24 +0300

    продолжаем движение

--- homework/module_2_3.py
+++ homework/module_2_3.py
@@ -1,9 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-while len(my_list) != 0:
-    value = my_list.pop(0)
-    if value > 0:
-        print(value)
-    elif value == 0:
-        continue
-    else:
+i = 0
+while len(my_list) > i :
+    if my_list[i] > 0:
+        print(my_list[i])
+    elif my_list[i] < 0:
         break
+    i += 1
